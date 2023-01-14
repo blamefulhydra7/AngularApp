9f163287379e431f340721a804a938bb250dfc82
Leonardo Solón Sánchez Flores <blamefulhydra@hotmail.com>
2023-01-13 19:10:41 -0700

    Primer commit de la app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,3 @@
+export interface titulo {
+    nombre: string
+}
